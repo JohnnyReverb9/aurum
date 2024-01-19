1b9f42b69ea7b51aae1ae5d7fbfe0ea68194a08d
Stepan <stepan.rulyov@gmail.com>
2024-01-19 23:56:43 +0300

    Подготовка структуры

---
+++ misc/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$title = "Dog's Docs";
+include_once __DIR__ . "/pages/header.php";
+
+?>
+
+<body>
+<div>
+    <h1>Welcome!</h1>
+    <span>Have ever been here?</span>
+    <a href="sign_in.php">Sign in</a>
+    <a href="sign_up.php">Sign up</a>
+</div>
+</body>
+
+<?php
+
+include_once __DIR__ . "/pages/footer.php";
+
+?>
---
+++ misc/pages/footer.php
@@ -0,0 +1,8 @@
+<br>
+<br>
+<br>
+
+<div>
+    <span>Footer copyright sign</span>
+</div>
+</html>
