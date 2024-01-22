abd87063a0052cfd6441ad09337b2780084e948d
Stepan <stepan.rulyov@gmail.com>
2024-01-22 23:56:57 +0300

    Формы регистрации и авторизации

--- misc/sign_in.php
+++ misc/sign_in.php
@@ -13,16 +13,11 @@ PagePartSelector::selectPart("navbar");
         <form action="../bin/auth_user.php" method="post" class="mt-4">
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your e-mail">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter your password">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
--- misc/sign_up.php
+++ misc/sign_up.php
@@ -7,28 +7,33 @@ PagePartSelector::selectPart("navbar");
 
 ?>
 <div class="container" style="margin: 100px;">
-    <div>
-        <form action="../bin/auth_user.php" method="post">
-            <label for="name">Name</label>
-            <label>
-                <input type="text" name="name" placeholder="Enter your name">
-            </label>
-            <label for="login">Login</label>
-            <label>
-                <input type="text" name="login" placeholder="Enter your login">
-            </label>
-            <label for="email">E-mail</label>
-            <label>
-                <input type="email" name="email" placeholder="Enter your e-mail">
-            </label>
-            <label>
-                <input type="password" name="password" placeholder="Enter your password">
-            </label>
-            <label for="password">Confirm password</label>
-            <label>
-                <input type="password" name="passwordConfirm" placeholder="Confirm your password">
-            </label>
-            <button>Sign up</button>
+        <h2>Sign up</h2>
+        <form action="../bin/auth_user.php" method="post" class="mt-4">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">User name</label>
+                <input type="text" class="form-control" id="username" aria-describedby="emailHelp" placeholder="Enter your name">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Login</label>
+                <input type="text" class="form-control" id="login" aria-describedby="emailHelp" placeholder="Enter your login">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter your e-mail">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">User avatar</label>
+                <input type="file" class="form-control" id="avatar" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Enter your password">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Confirm password</label>
+                <input type="password" class="form-control" id="passwordConfirm" placeholder="Confirm your password">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
 </div>
