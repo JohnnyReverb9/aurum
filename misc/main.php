346a62f3657e0eb0ef5933edc4ed0649b3ecac21
Stepan <stepan.rulyov@gmail.com>
2024-01-24 17:07:00 +0300

    Опечатка

--- misc/main.php
+++ misc/main.php
@@ -14,7 +14,7 @@ PagePartSelector::selectPart("navbar");
         <p class="lead">This is your personal workspace with cool name.</p>
         <hr class="my-4">
         <p>Here you can organize your work to build your effectiveness.</p>
-        <h3 class="display-6">You've never been here before?</h3>
+        <h3 class="display-6">Have you never been here before?</h3>
         <a class="btn btn-primary btn-lg" role="button" href="/sign_up">Sign up</a>
         <h3 class="display-6">Sign in to touch unknown pleasures...</h3>
         <a class="btn btn-primary btn-lg" role="button" href="/sign_in">Sign in</a>
